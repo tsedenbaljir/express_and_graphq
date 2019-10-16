6622c11b03238fa5d9cb07a59edde28526c5a0de
tsedenbaljir <tsedenbaljire@gmail.com>
2019-10-16 16:42:49 +0800

    Express Ба Graphql MONGODB Холболт Болсон

---
+++ config.js
@@ -0,0 +1,3 @@
+module.exports = {
+  MONGODB: 'mongodb+srv://admin:4eMlxxeKrJPe2jNm@cluster0-blnkq.mongodb.net/memg?retryWrites=true'
+}
---
+++ index.js
@@ -0,0 +1,48 @@
+const { ApolloServer } = require('apollo-server');
+const gql = require('graphql-tag');
+const mongoose = require('mongoose');
+
+const Post = require('./model/Post');
+const { MONGODB } = require('./config.js');
+
+const typeDefs = gql`
+  type Post {
+    id: ID!
+    body: String!
+    createdAt: String!
+    username: String!
+  }
+  type Query {
+    getPosts: [Post]
+    # sayHi: String!
+  }
+`;
+
+const resolvers = {
+  Query: {
+    async getPosts() {
+      try {
+        const posts = await Post.find();
+        return posts;
+      } catch (err) {
+        throw new Error(err);
+      }
+    }
+    // sayHi: () => 'Hello world'
+  }
+};
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers
+});
+ 
+mongoose
+  .connect(MONGODB, { useNewUrlParser: true })
+  .then(() => {
+    console.log('MongoDB Connected');
+    return server.listen({ port: 5000 });
+  })
+  .then((res) => {
+    console.log(`Server running at ${res.url}`);
+  });
